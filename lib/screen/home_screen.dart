75395c28eda6d964ed2cbd4411cf71f7ce42361a
Minhee Seo <hwarakhada@gmail.com>
2022-05-11 16:59:39 +0900

    SafeArea 위젯으로 홈 화면 범위 수정

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  // StatelessWidget를 상속받으려면 꼭! 오버라이드 해줘야하는게 있음
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Home Screen"),
+      body: SafeArea(
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            children: [
+              Container(
+                width: 50.0,
+                height: 50.0,
+                color: Colors.red,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                color: Colors.orange,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                color: Colors.green,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }

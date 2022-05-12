5c92755117049fa93e2428bdd1057d8adb858346
Minhee Seo <hwarakhada@gmail.com>
2022-05-12 10:22:31 +0900

    웹뷰 띄우기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,82 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-            color: Colors.black,
-            width: MediaQuery.of(context).size.width,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Container(
-                        width: 50.0,
-                        height: 50.0,
-                        color: Colors.red,
-                      ),
-                      Container(
-                        width: 50.0,
-                        height: 50.0,
-                        color: Colors.orange,
-                      ),
-                      Container(
-                        width: 50.0,
-                        height: 50.0,
-                        color: Colors.yellow,
-                      ),
-                      Container(
-                        width: 50.0,
-                        height: 50.0,
-                        color: Colors.green,
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  width: 50.0,
-                  height: 50.0,
-                  color: Colors.orange,
-                ),
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Container(
-                        width: 50.0,
-                        height: 50.0,
-                        color: Colors.red,
-                      ),
-                      Container(
-                        width: 50.0,
-                        height: 50.0,
-                        color: Colors.orange,
-                      ),
-                      Container(
-                        width: 50.0,
-                        height: 50.0,
-                        color: Colors.yellow,
-                      ),
-                      Container(
-                        width: 50.0,
-                        height: 50.0,
-                        color: Colors.green,
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  width: 50.0,
-                  height: 50.0,
-                  color: Colors.green,
-                ),
-              ],
-            )),
+    return Scaffold(
+      body: WebView(
+        initialUrl: "https://blog.codefactory.ai",
+        javascriptMode: JavascriptMode.unrestricted,
       ),
     );
   }

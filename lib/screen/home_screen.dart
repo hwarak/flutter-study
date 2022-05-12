47a50cb00652cf5a7ecc6674afe40b7d78461e67
Minhee Seo <hwarakhada@gmail.com>
2022-05-12 16:54:29 +0900

    폰트 세팅

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -10,13 +13,49 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Timer? timer;
+  PageController controller = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+
+    timer = Timer.periodic(Duration(seconds: 4), (timer) {
+      int currentPage = controller.page!.toInt();
+      int nextPage = currentPage + 1;
+
+      if (nextPage > 4) {
+        nextPage = 0;
+      }
+
+      controller.animateToPage(
+        nextPage,
+        duration: Duration(milliseconds: 400),
+        curve: Curves.linear,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    if (timer != null) {
+      timer!.cancel();
+    }
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
     return Scaffold(
       body: PageView(
+        controller: controller,
         children: [1, 2, 3, 4, 5]
             .map((e) => Image.asset(
-                  'assets/img/image_$e.jpeg',
+                  'assets/images/image_$e.jpeg',
                   fit: BoxFit.cover,
                 ))
             .toList(),

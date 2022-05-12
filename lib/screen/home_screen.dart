aecacbce6a355d9b9b05a5f1f6612b5018942da2
Minhee Seo <hwarakhada@gmail.com>
2022-05-12 13:06:32 +0900

    StatelessWidget -> StatefulWidget (1)  기본 세팅

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,38 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
-  WebViewController? controller; // null이 가능하게 선언을 해보자
-  final homeUrl = "https://blog.codefactory.ai";
-  HomeScreen({Key? key}) : super(key: key);
+  final Color color;
+
+  const HomeScreen({
+    required this.color,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("CodeFactory"),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {
-              if (controller == null) {
-                return;
-              }
-              controller!
-                  .loadUrl(homeUrl); // controller는 절대 null이 될 수 없다는걸 알려줘야함
-            },
-            icon: Icon(Icons.home),
-          )
-        ],
-        backgroundColor: Colors.orange,
-      ),
-      body: WebView(
-        onWebViewCreated: (WebViewController controller) {
-          this.controller = controller;
-        },
-        initialUrl: homeUrl,
-        javascriptMode: JavascriptMode.unrestricted,
-      ),
+    return Container(
+      width: 50.0,
+      height: 50.0,
+      color: color,
     );
   }
 }

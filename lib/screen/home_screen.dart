cf41cb15b9ac7d4336a8955f79d6afc879a127a0
Minhee Seo <hwarakhada@gmail.com>
2022-05-13 13:19:04 +0900

    const 사용해보기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,136 +1,50 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  DateTime selectedDate = DateTime(
-    DateTime.now().year,
-    DateTime.now().month,
-    DateTime.now().day,
-  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink[100],
-      body: SafeArea(
-        bottom: false,
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            children: [
-              _TopPart(
-                selectedDate: selectedDate,
-                onPressed: onHeartPressed,
-              ),
-              _BottomPart()
-            ],
-          ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const TestWidget(
+              // 우리가 build 타임에 모든 값들을 다 알 수 있을때만 const를 쓸 수 있다
+              // "test1" 이라는 글자는 build타임때 변경 안되니까 사용 가능
+              label: "test1",
+            ),
+            TestWidget(
+              label: "test2",
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {});
+                },
+                child: Text("build!!"))
+          ],
         ),
       ),
     );
   }
-
-  void onHeartPressed() {
-    showCupertinoDialog(
-      context: context,
-      barrierDismissible: true,
-      builder: (BuildContext context) {
-        return Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            color: Colors.white,
-            height: 300.0,
-            child: CupertinoDatePicker(
-              initialDateTime: selectedDate,
-              maximumDate: DateTime(
-                DateTime.now().year,
-                DateTime.now().month,
-                DateTime.now().day,
-              ),
-              mode: CupertinoDatePickerMode.date,
-              onDateTimeChanged: (DateTime date) {
-                setState(() {
-                  selectedDate = date;
-                });
-              },
-            ),
-          ),
-        );
-      },
-    );
-  }
 }
 
-class _TopPart extends StatelessWidget {
-  final DateTime selectedDate;
-  final VoidCallback onPressed;
-
-  _TopPart({
-    required this.selectedDate,
-    required this.onPressed,
-    Key? key,
-  }) : super(key: key);
+class TestWidget extends StatelessWidget {
+  final String label;
+  const TestWidget({required this.label, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final textTheme = theme.textTheme;
-    final now = DateTime.now();
-    return Expanded(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text(
-            "U&I",
-            style: textTheme.headline1,
-          ),
-          Column(
-            children: [
-              Text(
-                "우리 처음 만난 날",
-                style: textTheme.bodyText1,
-              ),
-              Text(
-                "${selectedDate.year}.${selectedDate.month}.${selectedDate.day}",
-                style: textTheme.bodyText2,
-              ),
-            ],
-          ),
-          IconButton(
-            iconSize: 60.0,
-            onPressed: onPressed,
-            icon: Icon(
-              Icons.favorite,
-              color: Colors.red,
-            ),
-          ),
-          Text(
-            "D+${DateTime(
-                  now.year,
-                  now.month,
-                  now.day,
-                ).difference(selectedDate).inDays + 1}",
-            style: textTheme.headline2,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class _BottomPart extends StatelessWidget {
-  const _BottomPart({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Image.asset('assets/images/middle_image.png'),
+    print('$label build 실행');
+    return Container(
+      child: Text(label),
     );
   }
 }

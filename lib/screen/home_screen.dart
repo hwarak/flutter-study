2a0e7f094289f98a0e36d35cad668fe04f087e4c
Minhee Seo <hwarakhada@gmail.com>
2022-05-13 16:29:10 +0900

    높이 조절

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -14,48 +14,59 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: PRIMARY_COLOR,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  "랜덤숫자 생성기",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.w700),
-                ),
-                IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.settings,
-                      color: RED_COLOR,
-                    ))
-              ],
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text("123"),
-                  Text("456"),
-                  Text("789"),
+                  const Text(
+                    "랜덤숫자 생성기",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.settings,
+                        color: RED_COLOR,
+                      ))
                 ],
               ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: RED_COLOR, //primary : 주 색상, 활성화가 되었을때 보이는 색상을 의미함
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: 123
+                          .toString()
+                          .split('')
+                          .map((e) => Image.asset('assets/images/$e.png'))
+                          .toList(),
+                    ),
+                    Row(),
+                    Row(),
+                  ],
                 ),
-                onPressed: () {},
-                child: const Text("생성하기"),
               ),
-            )
-          ],
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: RED_COLOR, //primary : 주 색상, 활성화가 되었을때 보이는 색상을 의미함
+                  ),
+                  onPressed: () {},
+                  child: const Text("생성하기"),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );

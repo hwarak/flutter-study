6a6079c038bf4a31846cbec2e4b553c02a21c8de
Minhee Seo <hwarakhada@gmail.com>
2022-05-16 13:36:36 +0900

    코드 정리

--- lib/screen/settings_screen.dart
+++ lib/screen/settings_screen.dart
@@ -10,6 +10,25 @@ class SettingsScreen extends StatefulWidget {
 
 class _SettingsScreenState extends State<SettingsScreen> {
   double maxNumber = 1000;
+
+  onButtonPressed() {
+    // 뒤로가기
+    // pop : 현재 화면을 터뜨리자!!
+    Navigator.of(context).pop(maxNumber.toInt());
+  }
+
+  onSliderChanged(double val) {
+    setState(() {
+      // onChanged가 불릴때마다, 즉  Slider의 값이 변경이 될 때마다
+      // maxNumber에 value를 저장을 하는데
+      // value를 저장 하면 build를 다시 실행하게 할거야
+      // build가 state에서 다시 실행이 되었으니 Slider도 다시 실행이 될거야
+      // 어떤값으로? 'maxNumber = val'이렇게 변경을 해놓은 'value: maxNumber' 이 value값으로 ! Slider를 다시 랜더링 함
+
+      maxNumber = val;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,29 +55,12 @@ class _SettingsScreenState extends State<SettingsScreen> {
       ),
     );
   }
-
-  onButtonPressed() {
-    // 뒤로가기
-    // pop : 현재 화면을 터뜨리자!!
-    Navigator.of(context).pop(maxNumber.toInt());
-  }
-
-  onSliderChanged(double val) {
-    setState(() {
-      // onChanged가 불릴때마다, 즉  Slider의 값이 변경이 될 때마다
-      // maxNumber에 value를 저장을 하는데
-      // value를 저장 하면 build를 다시 실행하게 할거야
-      // build가 state에서 다시 실행이 되었으니 Slider도 다시 실행이 될거야
-      // 어떤값으로? 'maxNumber = val'이렇게 변경을 해놓은 'value: maxNumber' 이 value값으로 ! Slider를 다시 랜더링 함
-
-      maxNumber = val;
-    });
-  }
 }
 
 class _Body extends StatelessWidget {
-  final double maxNumber;
   const _Body({required this.maxNumber, Key? key}) : super(key: key);
+
+  final double maxNumber;
 
   @override
   Widget build(BuildContext context) {
@@ -80,15 +82,16 @@ class _Body extends StatelessWidget {
 }
 
 class _Footer extends StatelessWidget {
-  final double maxNumber;
-  final VoidCallback onButtonPressed;
-  final ValueChanged<double>? onSliderChanged;
   const _Footer(
       {Key? key,
       required this.onButtonPressed,
       this.onSliderChanged,
       required this.maxNumber})
       : super(key: key);
+
+  final double maxNumber;
+  final VoidCallback onButtonPressed;
+  final ValueChanged<double>? onSliderChanged;
 
   @override
   Widget build(BuildContext context) {

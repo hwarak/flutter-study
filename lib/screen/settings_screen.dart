0e70b125af7ff802cb5c5b9b1af19c3e63f5865e
Minhee Seo <hwarakhada@gmail.com>
2022-05-16 12:57:49 +0900

    코드정리

--- lib/screen/settings_screen.dart
+++ lib/screen/settings_screen.dart
@@ -22,51 +22,93 @@ class _SettingsScreenState extends State<SettingsScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Expanded(
-                child: Row(
-                  children: maxNumber
-                      .toInt()
-                      .toString()
-                      .split('')
-                      .map((e) => Image.asset(
-                            'assets/images/$e.png',
-                            width: 50.0,
-                            height: 70.0,
-                          ))
-                      .toList(),
-                ),
+              _Body(
+                maxNumber: maxNumber,
               ),
-              Slider(
-                value: maxNumber,
-                min: 1000,
-                max: 100000,
-                onChanged: (double val) {
-                  setState(() {
-                    // onChanged가 불릴때마다, 즉  Slider의 값이 변경이 될 때마다
-                    // maxNumber에 value를 저장을 하는데
-                    // value를 저장 하면 build를 다시 실행하게 할거야
-                    // build가 state에서 다시 실행이 되었으니 Slider도 다시 실행이 될거야
-                    // 어떤값으로? 'maxNumber = val'이렇게 변경을 해놓은 'value: maxNumber' 이 value값으로 ! Slider를 다시 랜더링 함
-
-                    maxNumber = val;
-                  });
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  // 뒤로가기
-                  // pop : 현재 화면을 터뜨리자!!
-                  Navigator.of(context).pop(maxNumber.toInt());
-                },
-                child: Text('저장'),
-                style: ElevatedButton.styleFrom(
-                  primary: RED_COLOR,
-                ),
+              _Footer(
+                onButtonPressed: onButtonPressed,
+                onSliderChanged: onSliderChanged,
+                maxNumber: maxNumber,
               )
             ],
           ),
         ),
       ),
+    );
+  }
+
+  onButtonPressed() {
+    // 뒤로가기
+    // pop : 현재 화면을 터뜨리자!!
+    Navigator.of(context).pop(maxNumber.toInt());
+  }
+
+  onSliderChanged(double val) {
+    setState(() {
+      // onChanged가 불릴때마다, 즉  Slider의 값이 변경이 될 때마다
+      // maxNumber에 value를 저장을 하는데
+      // value를 저장 하면 build를 다시 실행하게 할거야
+      // build가 state에서 다시 실행이 되었으니 Slider도 다시 실행이 될거야
+      // 어떤값으로? 'maxNumber = val'이렇게 변경을 해놓은 'value: maxNumber' 이 value값으로 ! Slider를 다시 랜더링 함
+
+      maxNumber = val;
+    });
+  }
+}
+
+class _Body extends StatelessWidget {
+  final double maxNumber;
+  const _Body({required this.maxNumber, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(
+        children: maxNumber
+            .toInt()
+            .toString()
+            .split('')
+            .map((e) => Image.asset(
+                  'assets/images/$e.png',
+                  width: 50.0,
+                  height: 70.0,
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final double maxNumber;
+  final VoidCallback onButtonPressed;
+  final ValueChanged<double>? onSliderChanged;
+  const _Footer(
+      {Key? key,
+      required this.onButtonPressed,
+      this.onSliderChanged,
+      required this.maxNumber})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Slider(
+          value: maxNumber,
+          min: 1000,
+          max: 100000,
+          onChanged: onSliderChanged,
+        ),
+        ElevatedButton(
+          onPressed: onButtonPressed,
+          child: Text('저장'),
+          style: ElevatedButton.styleFrom(
+            primary: RED_COLOR,
+          ),
+        )
+      ],
     );
   }
 }

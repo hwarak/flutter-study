ff46d2cf9c520b2f3421b07f679e11d8097a6f59
Minhee Seo <hwarakhada@gmail.com>
2022-05-16 16:30:17 +0900

    MaterialStateProperty.resolveWith 사용

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -20,6 +20,42 @@ class _HomeScreenState extends State<HomeScreen> {
           children: [
             ElevatedButton(
               onPressed: () {},
+              child: Text("ButtonStyle"),
+              style: ButtonStyle(
+                // MaterialStateProperty 얘로 감싸줘야해
+                // all : 모든 상황에서 적용하겠다
+
+                // MaterialState
+                // 1. hovered - 호버링 상태(마우스 커서를 올려놓음)
+                // 2. focused - 포커스 됐을때 (텍스트 필드)
+                // 3. pressed - 눌렀을때 (O)
+                // 4. draged - 드래그 됐을때
+                // 5. selected - 선택됐을때(체크박스, 라디오버튼)
+                // 6. scrollUnder - 다른 컴포넌트 밑으로 스크롤링 됐을때
+                // 7. disabled - 비활성화 됐을 때 (O)
+                // 8. error - 에러상태
+
+                backgroundColor: MaterialStateProperty.resolveWith(
+                  (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.green;
+                    }
+                    return null;
+                  },
+                ),
+                foregroundColor: MaterialStateProperty.resolveWith(
+                  (Set<MaterialState> states) {
+                    // contains : 어떤값이 있는지 없는지 알 수 있다
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.white;
+                    }
+                    return Colors.red;
+                  },
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
               style: ElevatedButton.styleFrom(
                   // primary : 배경색? 아님. 메인컬러임
                   primary: Colors.red,

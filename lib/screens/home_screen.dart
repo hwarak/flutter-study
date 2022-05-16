d25126b70e2e945e1e8ff3360822171b13ba5afd
Minhee Seo <hwarakhada@gmail.com>
2022-05-16 15:35:59 +0900

    ElevatedButton 스타일링 적용

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,8 +11,45 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [],
+      appBar: AppBar(title: Text("버튼"), centerTitle: true),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  // primary : 배경색? 아님. 메인컬러임
+                  primary: Colors.red,
+                  // onPrimary : ElevatedButton에만 있음. 글자색과 클릭했을때의 애니메이션 효과 색이 정해진다.
+                  onPrimary: Colors.black,
+                  // shadowColor : 버튼 그림자 색
+                  shadowColor: Colors.green,
+                  // elevation : 3D 입체감의 높이
+                  elevation: 10.0,
+                  textStyle: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    fontSize: 20.0,
+                  ),
+                  padding: EdgeInsets.all(32.0),
+                  side: BorderSide(
+                    color: Colors.black,
+                    width: 4.0,
+                  )),
+              child: Text('ElevatedButton'),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: Text('OutlinedButton'),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text('TextButton'),
+            )
+          ],
+        ),
       ),
     );
   }

04f250f42f18cd8bb8dd2f319a30b133aec27dd1
Minhee Seo <hwarakhada@gmail.com>
2022-05-16 15:19:09 +0900

    splash 화면 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-    backgroundColor: Colors.black,
-    body: Center(
-      child: Text(
-        'Hello World',
-        style: TextStyle(color: Colors.white, fontSize: 20.0),
-      ),
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
     ),
-  )));
+  );
+}
+
+class HomeScreen extends StatelessWidget {
+  // StatelessWidget를 상속받으려면 꼭! 오버라이드 해줘야하는게 있음
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xfff99231),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/logo.png',
+          ),
+          CircularProgressIndicator(
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
 }

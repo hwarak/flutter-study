c9e4b6bc2557b77091d1558e6a6a70f8bbcc63cc
Minhee Seo <hwarakhada@gmail.com>
2022-05-17 14:09:45 +0900

    UI 완성

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,9 +10,49 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 30.0,
+      fontWeight: FontWeight.w300,
+    );
     return Scaffold(
-      body: Column(
-        children: [],
+      body: Container(
+        decoration: BoxDecoration(
+          // RadialGradient : 가운데부터 색이 동그랗게 퍼짐
+          // LinearGradient : 시작부터 끝까지 일괄적으로 색이 점차 바뀐다.
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF2A3A7C),
+              Color(0xff000118),
+            ],
+          ),
+        ),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/logo.png'),
+              SizedBox(
+                // 간격 줄려고 이렇게 쓰는 경우도 많음
+                height: 30.0,
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Text(
+                  "Video",
+                  style: textStyle,
+                ),
+                Text(
+                  "Player",
+                  // copyWith() : 이미 설정해놓은 값들은 유지를 하고 추가값들만 덮어씌우자!
+                  style: textStyle.copyWith(fontWeight: FontWeight.w700),
+                ),
+              ])
+            ],
+          ),
+        ),
       ),
     );
   }

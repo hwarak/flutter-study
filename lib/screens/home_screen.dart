5635137eb49f1dec75e69bb98cec187c766830b1
Minhee Seo <hwarakhada@gmail.com>
2022-05-17 14:14:45 +0900

    코드 정리

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,50 +10,74 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    final textStyle = TextStyle(
-      color: Colors.white,
-      fontSize: 30.0,
-      fontWeight: FontWeight.w300,
-    );
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
-          // RadialGradient : 가운데부터 색이 동그랗게 퍼짐
-          // LinearGradient : 시작부터 끝까지 일괄적으로 색이 점차 바뀐다.
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color(0xFF2A3A7C),
-              Color(0xff000118),
-            ],
-          ),
-        ),
+        decoration: getBoxdecoration(),
         child: SizedBox(
           width: MediaQuery.of(context).size.width,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset('assets/images/logo.png'),
+              _Logo(),
               SizedBox(
-                // 간격 줄려고 이렇게 쓰는 경우도 많음
-                height: 30.0,
-              ),
-              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                Text(
-                  "Video",
-                  style: textStyle,
-                ),
-                Text(
-                  "Player",
-                  // copyWith() : 이미 설정해놓은 값들은 유지를 하고 추가값들만 덮어씌우자!
-                  style: textStyle.copyWith(fontWeight: FontWeight.w700),
-                ),
-              ])
+                  // 간격 줄려고 이렇게 쓰는 경우도 많음
+                  height: 30.0),
+              _AppName()
             ],
           ),
         ),
       ),
+    );
+  }
+
+  BoxDecoration getBoxdecoration() {
+    return BoxDecoration(
+      // RadialGradient : 가운데부터 색이 동그랗게 퍼짐
+      // LinearGradient : 시작부터 끝까지 일괄적으로 색이 점차 바뀐다.
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color(0xFF2A3A7C),
+          Color(0xff000118),
+        ],
+      ),
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('assets/images/logo.png');
+  }
+}
+
+class _AppName extends StatelessWidget {
+  const _AppName({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 30.0,
+      fontWeight: FontWeight.w300,
+    );
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Video",
+          style: textStyle,
+        ),
+        Text(
+          "Player",
+          // copyWith() : 이미 설정해놓은 값들은 유지를 하고 추가값들만 덮어씌우자!
+          style: textStyle.copyWith(fontWeight: FontWeight.w700),
+        ),
+      ],
     );
   }
 }

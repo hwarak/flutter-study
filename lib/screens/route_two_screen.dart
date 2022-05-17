6eb1fc0df997677f4fa14b0b06214e946066b0be
Minhee Seo <hwarakhada@gmail.com>
2022-05-17 12:32:08 +0900

    pushAndRemoveUntil  사용

--- lib/screens/route_two_screen.dart
+++ lib/screens/route_two_screen.dart
@@ -59,6 +59,19 @@ class RouteTwo extends StatelessWidget {
           },
           child: Text("Push Replacement"),
         ),
+        ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (_) => RouteThree()),
+                // 스택에 [HomeScreen, RouteOne, RouteTwo, RouteThree] 이렇게 쌓여있는데
+                // RouteThree 전에 애들을 하나씩 비교하는거야
+                // route.settings.name이  "/"야?? 맞으면 냅두고 틀리면 삭제해!
+                // 그래서 결국 [HomeScreen, RouteThree] 이렇게 되고
+                // RouteThree에서 pop하면 HomeScreen으로 돌아감
+                (route) => route.settings.name == "/",
+              );
+            },
+            child: Text("Push And Remove Until"))
       ],
     );
   }

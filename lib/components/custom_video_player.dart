b804e047eab3a1abbaeafdaa31ca5a735ea13e75
Minhee Seo <hwarakhada@gmail.com>
2022-05-18 12:36:50 +0900

    아이콘 바깥쪽 눌러도 동작되는 버그 수정

--- lib/components/custom_video_player.dart
+++ lib/components/custom_video_player.dart
@@ -165,8 +165,8 @@ class _Controls extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height,
       color: Colors.black.withOpacity(0.5),
+      height: MediaQuery.of(context).size.height,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
